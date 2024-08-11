aa817b54196e198fc25ac88cb1e116f2188707a9
Grand Nabil <akambilawani@gmail.com>
2024-08-12 00:03:59 +0100

    fichier main.py pour la logique de l'assistant

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+incoming_msg = request.values.get('Body', '').lower()
+
+response = MessagingResponse()
+msg = response.message()
+msg.body("c'est une réponse de l'assistant virtuel.")
+
+def bot()
+
+    user_msg = request.values.get('Body', '').lower()
+
+    response = MessagingResponse()
+
